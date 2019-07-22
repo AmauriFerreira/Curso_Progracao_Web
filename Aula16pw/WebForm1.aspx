91e67860eadea95233cae9f8cd84f79616397340
Amauri A. Ferreira <48930008+AmauriFerreira@users.noreply.github.com>
2019-07-22 20:58:49 -0300

    Add files via upload

---
+++ Aula16pw/WebForm1.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="pw16_correcao.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h1>Orçamento</h1>
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            Nome:
+            <asp:TextBox ID="txtNome" runat="server"></asp:TextBox>
+            <br />Telefone:
+            <asp:TextBox ID="txtTelefone" runat="server"></asp:TextBox>
+            <br />Produto: 
+            <asp:DropDownList ID="cboProduto" runat="server"></asp:DropDownList>
+            <br />Qtd:
+            <asp:TextBox ID="txtQtd" TextMode="Number" runat="server"></asp:TextBox>
+            <br />Mensagem:
+            <asp:TextBox ID="txtMensagem" TextMode="MultiLine" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Enviar" OnClick="enviar" />
+            <asp:Label ID="lblMensagem" runat="server"></asp:Label>
+        </ContentTemplate>
+        </asp:UpdatePanel>
+        <asp:UpdateProgress ID="UpdateProgress1" runat="server">
+            <ProgressTemplate>Enviando......</ProgressTemplate>
+        </asp:UpdateProgress>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Aula16pw/WebForm1.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.Net;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace pw16_correcao
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                carregarCombo();
+            }
+        }
+        private void carregarCombo()
+        {
+            String sc = "Server=localhost;Database=pweb;Uid=root;Pwd=;";
+            DataSet ds = new DataSet();
+            MySqlConnection con = new MySqlConnection(sc);
+            con.Open();
+            String sql = "select titulo from livro order by titulo";
+            MySqlDataAdapter conversor = new MySqlDataAdapter(sql, con);
+            conversor.Fill(ds);
+            con.Close();
+            cboProduto.DataSource = ds.Tables[0];
+            cboProduto.DataTextField = "titulo";
+            cboProduto.DataBind();
+        }
+        protected void enviar(object sender, EventArgs e)
+        {
+            MailAddress de = new MailAddress("fatecpwAds2016@outlook.com");
+            MailAddress para = new MailAddress("norton.glaser@fatec.sp.gov.br");
+            String mensagem = "<b>Nome:</b>" + txtNome.Text + "<br/>" +
+                    "<b>Telefone:</b>" + txtTelefone.Text + "<br/>" +
+                    "<b>Produto:</b>" + cboProduto.SelectedValue + "<br/>" +
+                    "<b>Qtd:</b>" + txtQtd.Text + "<br/>" +
+                    "<b>Mensagem:</b>" + txtMensagem.Text;
+ 
+            MailMessage email = new MailMessage(de, para);
+            email.Subject = "Orçamento";
+            email.Body = mensagem;
+            email.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient("smtp-mail.outlook.com");
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.Credentials = new
+            NetworkCredential("fatecpwAds2016@outlook.com", "FreiJoao59");
+            try
+            {
+                smtp.Send(email);
+                lblMensagem.Text = "Email enviado com sucesso !";
+            }
+            catch (Exception err)
+            {
+                lblMensagem.Text = "Ocorreu um erro ! " + err.Message;
+            }
+
+        }
+    }
+}
